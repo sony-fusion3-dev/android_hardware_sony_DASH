d9c14ce45c8aa272657d409b422c2d743e4ae663
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-04-01 15:59:26 +0000

    DASH: add support for lm3533 light sensor
    Change-Id: Ie0c76daddd15fe731d38ec25707f2ab240bb75da

--- sensors/Sensors.mk
+++ sensors/Sensors.mk
@@ -69,6 +69,7 @@ $(yes-var-compass-ak896xna)-files += ak896xna.c wrappers/ak896xna_sensors.c
 #
 
 $(SOMC_CFG_SENSORS_LIGHT_AS3676)-files += as3676_als.c
+$(SOMC_CFG_SENSORS_LIGHT_LM3533)-files += lm3533_als.c
 
 $(SOMC_CFG_SENSORS_LIGHT_LIBALS)-files += light_sensor_als.c
 $(SOMC_CFG_SENSORS_LIGHT_LIBALS)-c-includes += $(DASH_ROOT)/libs/libals
