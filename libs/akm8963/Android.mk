641013f3efb819d88e294fe1ed67f10ab6cbe121
Adrian DC <radian.dc@gmail.com>
2016-07-09 17:55:39 +0200

    akm8972: Make compilation of dummy library optional
     * Based upon commit from Bjorn Andersson,
        If6b87696229edc6ae297f20e8003cc6207f18a55
    
     * Also add the config check to akm8963 like akm8972
    
    Change-Id: Iad65e6071142cf0c0be534a049ada19da070511d

--- libs/akm8963/Android.mk
+++ libs/akm8963/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(SOMC_CFG_SENSORS_COMPASS_AK8963),yes)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -8,5 +10,7 @@ LOCAL_MODULE := libsensors_akm8963
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES += ak8963_dummy.c
 include $(BUILD_SHARED_LIBRARY)
+
+endif
 
 endif
--- libs/akm8972/Android.mk
+++ libs/akm8972/Android.mk
@@ -1,12 +1,16 @@
-ifeq ($(SOMC_CFG_SENSORS_COMPASS_AK8972),yes) 
+ifeq ($(SOMC_CFG_SENSORS_COMPASS_AK8972),yes)
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifneq ($(SOMC_CFG_SENSORS_AKM8972_DUMMY),)
+
 LOCAL_MODULE := libsensors_akm8972
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES += libsensors_akm8972_dummy.c
 include $(BUILD_SHARED_LIBRARY)
+
+endif
 
 endif
